b17922d3fd24288df0c3cfd5944666dfcea15173
DelFaa365 <Tomasinka123@gmail.com>
2022-06-01 12:57:59 +0200

    yes

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -7,16 +8,46 @@ public class App {
         Scanner sc = new Scanner(System.in);
         System.out.println("Zadej větu: ");
         String veta = sc.nextLine();
+        System.out.println("Zadej písmeno, který chceš na začátek");
+        String z = sc.nextLine();
 
-        char [] split = veta.toCharArray();
-        int pocet = 0;
-        for (char c : split) {
-            if(c <= 122 && c >= 97){
-                pocet++;
+        String [] split = veta.split(" ");
+        char [] spliticek = veta.toCharArray();
+        char [] toPismenoKteryChci = z.toCharArray();
+        int pocetSlovNaPismeno = 0;
+        for (int i = 0; i < spliticek.length; i++) {
+            for (char c : toPismenoKteryChci) {
+                if(spliticek[i] == c){
+                    pocetSlovNaPismeno++;
+                }
             }
         }
-        System.out.println(pocet);
 
+        System.out.println("Počet slov ve větě: " + split.length);
+        System.out.println("Počet slov začínající na písmeno "+ z + ": " + pocetSlovNaPismeno);
 
+        System.out.println("Zadej kolikrát hodíš kostkou");
+        int kolikratHod = sc.nextInt();
+        System.out.println("Zadej na jaký x-tý hod číslo padlo");
+        int naXHod = sc.nextInt();
+        int koncoveCislo = 0;
+        int pocetKoncovehoCisla = 0;
+        Random random = new Random();
+        int [] cislaKterPadly = new int[kolikratHod];
+        for (int i = 0; i < kolikratHod; i++) {
+            int nahodneCislo = random.nextInt(7);
+            cislaKterPadly[i] = nahodneCislo;
+            if(i == naXHod-1){
+                koncoveCislo = nahodneCislo;
+            }
+        }
+
+        for (int i : cislaKterPadly) {
+            if(i == koncoveCislo){
+                pocetKoncovehoCisla++;
+            }
+        }
+        System.out.println("Číslo které ti padlo: " + koncoveCislo);
+        System.out.println("Kolikrát toto číslo padlo: " + pocetKoncovehoCisla);
     }
 }
